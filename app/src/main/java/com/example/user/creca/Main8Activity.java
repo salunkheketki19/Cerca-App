6cd6fe9b84ad141b7540fb154e412ac4082a5358
Ketki <ketkisalunkhe333@gmail.com>
2018-05-19 16:11:24 +0530

    Added all the files

---
+++ app/src/main/java/com/example/user/creca/Main8Activity.java
@@ -0,0 +1,15 @@
+package com.example.user.creca;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.user.creca.R;
+
+
+public class Main8Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main8);
+    }
+}
